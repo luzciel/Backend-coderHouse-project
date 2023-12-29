fcccc3cd7cedef4b4bf7bae70d13a2adf4a84931
luzciel <luzcielmontesinos@gmail.com>
2023-12-29 17:48:38 -0300

    Feature: the document path is saved in the database

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -3,11 +3,13 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   
   destination: (req, file, cb) => {
-    cb(null, __dirname + "/../../src/public/uploads");
+    const uploadPath = __dirname + "/../../src/public/uploads";
+
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname + "-" + uniqueSuffix);
+    cb(null, uniqueSuffix + "-" + file.originalname);
   },
 });
 
